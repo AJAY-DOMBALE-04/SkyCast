72da9cad332eabfc1b772564c04ed064eb66aa70
AJAY-9604 <ajaydombale4@gmail.com>
2024-11-17 12:39:40 +0530

    Add files via upload

---
+++ apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "0cf5600110861ce3dde26d82a41db416",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
